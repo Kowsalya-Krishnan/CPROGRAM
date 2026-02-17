2376b2edbaf51313fb749e7e161ba27a626d466a
Kowsalya-Krishnan <kowsalyakrishnan460@gmail.com>
2026-02-17 12:00:08 +0530

    Add files via upload

---
+++ ConditionalStatements/C program2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int percentage;
+    scanf("%d",&percentage);
+    if(percentage>=95){
+        printf("A");
+    }
+    else if(percentage>=85){
+        printf("B");
+        
+    }
+    else if(percentage>=75){
+        printf("c");
+    }
+    else if(percentage>=65){
+        printf("D");
+        
+    }
+    else if(percentage>=45){
+        printf("E");
+    }
+    else{
+        printf("F");
+    }
+}
