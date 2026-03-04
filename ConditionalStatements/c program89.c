7f64031385c3f9f3a0695511d56aaf215a73da7a
Kowsalya Krishnan <kowsalyakrishnan460@gmail.com>
2026-03-04 21:58:12 +0530

    Add files via upload

---
+++ ConditionalStatements/c program89.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int chef,chefina;
+    int sum;
+    scanf("%d",&chef,&chefina);
+    sum=chef+chefina;
+    if(sum>6){
+        printf("YES");
+        
+    }
+    else{
+        printf("No");
+    }
+}
